7ae823efc9ca2ae404d53ef83c9b98b9b59ab859
Daniil Yakovlev <k1ng1402@MacBook-Pro-Daniil.local>
2021-09-02 20:43:03 +0300

    product page without functionality and with small bugs

--- assets/theme.js
+++ assets/theme.js
@@ -1,3 +1,5 @@
+window.theme = window.theme || {}
+
 const openMenuBtn = document.querySelector(".open__menu")
 const mobileMenu = document.querySelector(".mobile__menu")
 const closeMenuBtn = mobileMenu.querySelector(".close__menu")
@@ -14,4 +16,55 @@ const menuShow = function () {
 
 openMenuBtn.addEventListener("click", function () {
   menuShow()
+})
+
+theme.carouseles = (function () {
+	const owlCarouseles = [
+			{
+				selector: "#product-slider",
+				getSettings: function () {
+					return {
+						nav: $(this.selector).data("nav"),
+						dots: $(this.selector).data("dots"),
+						loop: $(this.selector).data("loop"),
+						items: $(this.selector).data("items"),
+						autoplay: $(this.selector).data("autoplay"),
+						mouseDrag: false,
+						autoplayHoverPause: true,
+						navText: ["<span></span>", "<span></span>"],
+						dotsContainer: "#product-slider-dots",
+						onInitialized: function (event) {
+							const target = event.target.id ? event.target.id : event.target.classList[0]
+							console.log(`${target} has been initialized.`)
+							$("#product-slider-dots .owl-dot").click(function () {
+								// console.log($(this).index());
+								$("#product-slider").trigger("to.owl.carousel", [$(this).index(), 300])
+							})
+						},
+					}
+				},
+			},
+		],
+		initializeOwlCarousel = (carousel) => ($(carousel.selector).length > 0 ? $(carousel.selector).owlCarousel(carousel.getSettings()) : false)
+	reInitializeOwlCarousel = (selector) => {
+		$(selector).trigger("destroy.owl.carousel")
+		initializeOwlCarousel(owlCarouseles.filter((carousel) => carousel.selector == selector)[0])
+	}
+
+	// Initializing
+	const init = function () {
+		owlCarouseles.forEach(initializeOwlCarousel)
+	}
+	return {
+		init,
+		reInitializeOwlCarousel,
+	}
+})()
+
+theme.init = function () {
+	theme.carouseles.init()
+}
+
+$(function () {
+	theme.init()
 })
