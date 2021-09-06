66d5c2ba827743d33e7a7ffa03ee08a8cd8f2713
Daniil Yakovlev <k1ng1402@MacBook-Pro-Daniil.local>
2021-09-06 17:17:34 +0300

    added ajax basket functionality, fixed bugs

--- assets/cartajax.js
+++ assets/cartajax.js
@@ -1,12 +1,160 @@
-//********* changing the quantity of the product *********//
-const quantityCartBlock = document.querySelector(".cart__item-qwt")
-const quantityCartItem = quantityCartBlock.querySelector(".quantity")
+const addToBasket = document.querySelector("#add-to-basket")
+const cartAjaxSection = document.querySelector(".cart-ajax")
 
-quantityCartBlock.addEventListener("click", function (evt) {
-	let pointer = evt.target
-	if (pointer.classList.contains("qwt__minus") && quantityCartItem.value != 1) {
-		quantityCartItem.value = `${quantityCartItem.value - 1}`
-	}else if (pointer.classList.contains("qwt__plus")) {
-		quantityCartItem.value = `${Number(quantityCartItem.value) + 1}`
-	}
-});
+
+//********* changing the cart count for cart btn *********//
+const cartBtn = document.querySelector(".cart__btn")
+
+const changeCartCount = function (quantity) {
+  const cartCount = document.querySelector(".cart__count")
+  if (!cartCount) {
+    const cartCount = document.createElement("span")
+    cartCount.classList.add("cart__count")
+    cartCount.textContent = `${quantity}`
+    cartBtn.appendChild(cartCount)
+  }else {
+    cartCount.textContent = `${quantity}`
+    if (quantity == 0) {
+      cartCount.remove()
+    }
+  } 
+}
+
+//********* update ajax cart *********//
+const updateCart = function (section) {
+	fetch(`/?sections=${section}`)
+    .then((response) => response.json())
+		.then((data) => {
+
+			const currentCartSection = document.querySelector(".cart-ajax")
+      const currentCartCount = currentCartSection.querySelector(".cart__counter")
+      const currentCartPrice = currentCartSection.querySelector(".cart__price-total")
+      const currentCartMain = currentCartSection.querySelector(".cart__main")
+
+			const updatedCart = new DOMParser().parseFromString(data.cartajax, "text/html")
+
+			const updatedCartSection = updatedCart.querySelector(".cart-ajax")
+      const updatedCartCount = updatedCartSection.querySelector(".cart__counter")
+      const updatedCartPrice = updatedCartSection.querySelector(".cart__price-total")
+      const updatedCartMain = updatedCartSection.querySelector(".cart__main")
+
+      currentCartCount.replaceWith(updatedCartCount)
+      currentCartPrice.replaceWith(updatedCartPrice)
+      currentCartMain.replaceWith(updatedCartMain)
+
+      
+      if (!currentCartSection.classList.contains("active")) {
+        currentCartSection.classList.add("active")
+      }
+      let tmp = updatedCartCount.querySelector("span")
+      changeCartCount(tmp.textContent)
+		})
+}
+
+//********* changing product in the cart *********//
+const updateItem = function (id, quantity) {
+  const updatedCart = new DOMParser().parseFromString(`<div class="ring-loader"></div>`, "text/html")
+  const loader = updatedCart.querySelector(".ring-loader")
+  const cartItems = cartAjaxSection.querySelector(".cart__items")
+  cartItems.replaceWith(loader)
+
+  fetch("/cart/change.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, quantity }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      if (response.status === 422) {
+        throw "Out of Stock!"
+      } else if (response.status === 404) {
+        throw "Can not find variant!"
+      } else {
+        throw "Something went wrong. Please reload the page!"
+      }
+    }
+  })
+  .then((data) => {
+    
+    updateCart("cartajax", quantity)
+    console.log("/cart/change.js", data)
+  })
+
+
+  
+}
+
+//********* add item in the cart *********//
+const addItem = function (id, quantity) {
+
+  fetch("/cart/add.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: [
+        {
+          id: Number(id),
+          quantity: quantity,
+        },
+      ],
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        if (response.status === 422) {
+          throw "Out of Stock!"
+        } else if (response.status === 404) {
+          throw "Can not find variant!"
+        } else {
+          throw "Something went wrong. Please reload the page!"
+        }
+      }
+      return response.json()
+    })
+    .then((data) => {
+      updateCart("cartajax")
+      console.log("/cart/add.js", data)
+    })
+    .catch((e) => {
+      console.error("/cart/add.js", e)
+    })
+}
+
+
+if (addToBasket) {
+  addToBasket.addEventListener("click", function (evt) {
+    evt.preventDefault()
+    let pointer = evt.target
+    addItem(pointer.dataset.variantId, pointer.dataset.variantQuantity)
+  })
+}
+
+//********* changing the quantity of the product in the cart *********//
+cartAjaxSection.addEventListener("click", function (evt) {
+  let pointer = evt.target
+  if (pointer.classList.contains("quantity__minus")) {
+
+    let quantityBlock = pointer.parentElement
+    let quantity = quantityBlock.querySelector(".quantity")
+
+    if (quantity.value != 0) {
+
+      quantity.value = `${Number(quantity.value) - 1}`
+      let itemId = quantity.getAttribute("data-item-id")
+      updateItem(itemId, quantity.value)
+
+    }
+  }else if (pointer.classList.contains("quantity__plus")) {
+
+    let quantityBlock = pointer.parentElement
+    let quantity = quantityBlock.querySelector(".quantity")
+
+    quantity.value = `${Number(quantity.value) + 1}`
+    let itemId = quantity.getAttribute("data-item-id")
+    updateItem(itemId, quantity.value)
+  }
+}) 
--- assets/product.js
+++ assets/product.js
@@ -1,18 +1,18 @@
 //********* changing the quantity of the product *********//
-const quantityBlock = document.querySelector(".qwt")
+const quantityBlock = document.querySelector(".quantity__block")
 const quantity = quantityBlock.querySelector(".quantity")
-const addToBasket = document.querySelector("#add-to-basket")
 
 quantityBlock.addEventListener("click", function (evt) {
 	let pointer = evt.target
-	if (pointer.classList.contains("qwt__minus") && quantity.value != 1) {
+	if (pointer.classList.contains("quantity__minus") && quantity.value != 1) {
 		quantity.value = `${quantity.value - 1}`
-    addToBasket.setAttribute("data-variant-quantity", quantity.value)
-	}else if (pointer.classList.contains("qwt__plus")) {
+    addToBasket.dataset.variantQuantity = quantity.value
+	}else if (pointer.classList.contains("quantity__plus")) {
 		quantity.value = `${Number(quantity.value) + 1}`
-    addToBasket.setAttribute("data-variant-quantity", quantity.value)
+    addToBasket.dataset.variantQuantity = quantity.value
 	}
-});
+})
+
 
 //********* changing the price of the product *********//
 const optionSelect = document.querySelector(".option__select")
@@ -23,67 +23,4 @@ optionSelect.addEventListener("change", function () {
   const priceValue = selectedOption.dataset.variantPrice
   addToBasket.dataset.variantId = optionSelect.value
   price.textContent = `${priceValue}`
-})
-
-//********* changing the cart count for cart btn *********//
-
-const cartBtn = document.querySelector(".cart__btn")
-
-
-const changeCartCount = function (quantity) {
-  const cartCount = document.querySelector(".cart__count")
-  if (!cartCount) {
-    let cartCount = document.createElement("span")
-    cartCount.classList.add("cart__count")
-    cartCount.textContent = `${quantity}`
-    cartBtn.appendChild(cartCount)
-  }else {
-    cartCount.textContent = `${Number(cartCount.textContent) + parseInt(quantity)}`
-  } 
-  
-}
-
-
-
-const addItem = (id, quantity) => {
-
-  fetch("/cart/add.js", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      items: [
-        {
-          id: Number(id),
-          quantity: quantity,
-        },
-      ],
-    }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status === 422) {
-          throw "Out of Stock!"
-        } else if (response.status === 404) {
-          throw "Can not find variant!"
-        } else {
-          throw "Something went wrong. Please reload the page!"
-        }
-      }
-      return response.json()
-    })
-    .then((data) => {
-      changeCartCount(quantity);
-      console.log("/cart/add.js", data)
-    })
-    .catch((e) => {
-      console.error("/cart/add.js", e)
-    })
-}
-
-addToBasket.addEventListener("click", function (evt) {
-  evt.preventDefault()
-  let pointer = evt.target
-  addItem(pointer.dataset.variantId, pointer.dataset.variantQuantity)
 })
